70fb76f158420e0afa549b978e7951146cafae34
andrewdigiacomo27 <andrewdigiacomo27@gmail.com>
2026-01-28 21:26:01 -0600

    attempt 3

--- assignment1.py
+++ assignment1.py
@@ -10,7 +10,7 @@ testData = pd.read_csv("assignment_data_test.csv")
 trainData['Timestamp'] = pd.to_datetime(trainData['Timestamp'])
 trainData.set_index(pd.DatetimeIndex(trainData['Timestamp']), inplace=True)
 
-varData = trainData[['trips','month', 'day', 'hour']].dropna()[:-50]
+varData = trainData[['trips','hour']].dropna()[:-50]
 model = VAR(varData)
 
 lag = model.select_order()
@@ -19,7 +19,8 @@ print(lag.summary())
 modelFit = model.fit(lag.aic)
 
 nPeriods = varData.values[-modelFit.k_ar:]
-pred = modelFit.forecast(nPeriods, steps = 50)
+pred = modelFit.forecast(nPeriods, steps = 31*24)
+pred = pred[:,0]
 
 
 
