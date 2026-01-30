ad64f4dfbdec9b420944adc46233a78aba59cf4d
andrewdigiacomo27 <andrewdigiacomo27@gmail.com>
2026-01-30 00:25:24 -0600

    attempt 5

--- assignment1.py
+++ assignment1.py
@@ -1,42 +1,21 @@
 import pandas as pd
+from statsmodels.tsa.api import ExponentialSmoothing
+import plotly.express as px
 import numpy as np
-from statsmodels.tsa.statespace.varmax import VARMAX
 
-# Load data
+#this should work. need to find a way to adjust accuracy
+
 trainData = pd.read_csv("assignment_data_train.csv")
 testData = pd.read_csv("assignment_data_test.csv")
 
 trainData['Timestamp'] = pd.to_datetime(trainData['Timestamp'])
-testData['Timestamp'] = pd.to_datetime(testData['Timestamp'])
+trainTrips = trainData['trips']
 
-# Endogenous variables (VAR part)
-endog = trainData[['trips', 'hour']]
+#maybe remove seasonality is causing error? test RMSE
+model = ExponentialSmoothing(trainTrips, trend='add')
 
-# Exogenous variables (X part)
-exog = trainData[['month', 'day']]
+modelFit = model.fit(optimized = True)
 
-# --------------------
-# Define model
-# --------------------
-model = VARMAX(
-    endog,
-    exog=exog,
-    order=(1, 1),      # VARMA(1,1)
-    trend='c'
-)
-
-# Fit model
-modelFit = model.fit(disp=False)
-
-# --------------------
-# Forecast January
-# --------------------
-exog_future = testData[['month', 'day']]
-
-forecast = modelFit.forecast(
-    steps=744,
-    exog=exog_future
-)
-
-# Extract trips forecast only
-pred = forecast['trips'].values
+forecast1 = len(testData)
+pred = modelFit.forecast(forecast1)
+pred = np.array(pred)
