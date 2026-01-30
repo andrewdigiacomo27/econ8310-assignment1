1889a70a20eafff1a8f6070a09f979b0f9c23eb6
andrewdigiacomo27 <andrewdigiacomo27@gmail.com>
2026-01-30 16:09:38 -0600

    attempt 7

--- assignment1.py
+++ assignment1.py
@@ -1,23 +1,35 @@
-import pandas as pd
-from statsmodels.tsa.api import ExponentialSmoothing
-import plotly.express as px
-import numpy as np
+#GAM model - seems too detailed for this, did not complete
 
-#this should work. need to find a way to adjust accuracy
+from pygam import LinearGAM, s, f, l
+import patsy as pt
+
+import pandas as pd
+import numpy as np
+from plotly import tools
+import plotly.offline as py
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+
 
 trainData = pd.read_csv("assignment_data_train.csv")
 testData = pd.read_csv("assignment_data_test.csv")
 
-trainData['Timestamp'] = pd.to_datetime(trainData['Timestamp'])
-trainTrips = trainData['trips']
+# #generate x and y matrices
+# eqn = """trips ~ -1 + year + month + day + hour"""
+# y, x = pt.dmatrices(eqn, data = trainData)
 
-#maybe remove seasonality is causing error? test RMSE
-model = ExponentialSmoothing(trainTrips, trend = 'add', seasonal = 'add', seasonal_periods = 24*7, damped_trend=True)
+#or can i do it this way
+y = trainData['trips'].values
+x = trainData[['year', 'month', 'day', 'hour']]
 
-modelFit = model.fit(optimized = True)
+xTest = testData[['year', 'month', 'day', 'hour']]
 
-forecast1 = len(testData)
-pred = modelFit.forecast(forecast1)
-pred = np.array(pred)
+#initializing and fit the model
+model = LinearGAM(s(0) + s(1) + s(2) + s(3))
+modelFit = model.gridsearch(np.asarray(x), y)
+
+predict = modelFit.predict(xTest)
 
 
+# rmse = np.sqrt(np.mean((pred - testData['trips'].values)**2))
+# print("RMSE:", rmse)
